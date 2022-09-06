c745de091f5c7823a10bbbbf35be9a2204df2195
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-06 20:15:05 -0300

    Estruturea da aplicacao - CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
 
 export function App() {
   return (
     <>
+      <Header />
+
       <Post
         author="John"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi veniam nam architecto minus dolorem facilis ea magnam aliquid eaque. Reiciendis earum harum magnam? Dolor omnis velit cum voluptate ipsam voluptas."
