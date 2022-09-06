cf73e6d35e20df7450a90a83a8dfed0def933b89
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-06 19:32:22 -0300

    Iniciando com react - Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import { Post } from "./Post"
 export function App() {
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="John"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi veniam nam architecto minus dolorem facilis ea magnam aliquid eaque. Reiciendis earum harum magnam? Dolor omnis velit cum voluptate ipsam voluptas."
+      />
+      <Post author="Agleylson" content="A new post like" />
     </>
   )
 }
