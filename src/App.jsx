03da8bfadd8690a96509ae0772963f5c2a128afd
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-07 16:45:26 -0300

    Estrutura da aplicacao - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from "./app.module.css"
 
 export function App() {
   return (
     <>
       <Header />
 
-      <Post
-        author="John"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi veniam nam architecto minus dolorem facilis ea magnam aliquid eaque. Reiciendis earum harum magnam? Dolor omnis velit cum voluptate ipsam voluptas."
-      />
-      <Post author="Agleylson" content="A new post like" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="John"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi veniam nam architecto minus dolorem facilis ea magnam aliquid eaque. Reiciendis earum harum magnam? Dolor omnis velit cum voluptate ipsam voluptas."
+          />
+          <Post author="Agleylson" content="A new post like" />
+        </main>
+      </div>
     </>
   )
 }
