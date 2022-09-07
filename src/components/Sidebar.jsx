4d4b6078815189ee5fd0d41e2246be4b1c776982
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-07 18:23:37 -0300

    Estrutura da aplicacao - Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react"
+import { Avatar } from "./Avatar"
 
 import styles from "./Sidebar.module.css"
 
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img src="https://github.com/agleymelo.png" />
+        <Avatar src="https://github.com/agleymelo.png" />
 
         <strong>Agleylson Melo</strong>
         <span>Web Developer</span>
