110829b7d27f1110387d5c872a243df35432924c
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-07 17:53:38 -0300

    Estrutura da aplicacao - Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react"
+import styles from "./Comment.module.css"
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/agleymelo.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Agleylson Melo</strong>
+
+              <time title="11 de Maio as 08:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 1 hora atras
+              </time>
+            </div>
+
+            <button title="Deletar comentario">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} /> Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
