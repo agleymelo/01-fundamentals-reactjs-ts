9653a26cbd83dc0192f5d68cf8db9c011513dca6
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-07 17:17:18 -0300

    Estrutura da aplicacao - Component: Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Post } from "./components/Post"
 import { Sidebar } from "./components/Sidebar"
 
 import styles from "./app.module.css"
@@ -17,7 +17,6 @@ export function App() {
             author="John"
             content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi veniam nam architecto minus dolorem facilis ea magnam aliquid eaque. Reiciendis earum harum magnam? Dolor omnis velit cum voluptate ipsam voluptas."
           />
-          <Post author="Agleylson" content="A new post like" />
         </main>
       </div>
     </>
---
+++ src/components/Post.jsx
@@ -0,0 +1,43 @@
+import styles from "./Post.module.css"
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/agleymelo.png"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Agleylson Melo</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio as 08:13" dateTime="2022-05-11 08:13:30">
+          Publicado ha 1 Hora
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
