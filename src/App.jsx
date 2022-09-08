2efa2a0d135f2160dd616d2e567ee8f2e6fa541e
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-08 18:38:12 -0300

    Os motores do React - Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,57 @@ import { Sidebar } from "./components/Sidebar"
 
 import styles from "./app.module.css"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatar_url: "https://github.com/agleymelo.png",
+      name: "Agleylson Melo",
+      role: "CEO @AgleyCompany",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    published_at: new Date("2022-09-03 20:30:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatar_url: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @AgleyCompany",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    published_at: new Date("2022-09-07 20:30:00"),
+  },
+]
+
 export function App() {
   return (
     <>
@@ -13,8 +64,15 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                published_at={post.published_at}
+              />
+            )
+          })}
         </main>
       </div>
     </>
