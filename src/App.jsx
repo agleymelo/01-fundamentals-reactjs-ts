13fc816f6c614911f2d7fb84438204335683bd39
Agleylson Melo <agleylsonmelo@outlook.com>
2022-09-10 09:56:46 -0300

    Os motores do React - Entendendo a key

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,7 @@ export function App() {
           {posts.map((post) => {
             return (
               <Post
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 published_at={post.published_at}
